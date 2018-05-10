70d164e9813975b652355556c16724b309ce2f87
Adima <777adima@bk.ru>
2018-05-10 22:46:38 +0400

    Test1 done

---
+++ TestProject/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using МодТест;
+
+namespace TestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int val = 100;
+            string expected = "100";
+
+            MyInt lol = new MyInt();
+            string answer = lol.ToStr(val);
+
+
+            Assert.AreEqual(expected, answer);
+        }
+    }
+}
---
+++ МодТест/MyInt.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace МодТест
+{
+    public class MyInt
+    {
+        public string Value = "";
+
+        //Конструкторы
+        public MyInt()
+        {
+            Value = "";
+        }
+
+        public string ToStr(int val)
+        {
+            bool isMin = false;
+            if (val < 0)
+            {
+                isMin = true;
+                val = -val;
+            }
+
+
+            string result = "";
+
+            long div = 10;
+            long ost = 0;
+            long dep = val;
+
+            for (int i = 0; dep > 0; i++)
+            {
+                ost = dep % div;
+                result = ost + result;
+                dep = dep / div;
+
+            }
+
+            if (isMin) result = "-" + result;
+            return result;
+
+        }
+    }
+}
